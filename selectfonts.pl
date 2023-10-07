4cfa1238a086ba63f5057ade73aab9a9da6c245a
Boris Veytsman <bveytsman@chanzuckerberg.com>
2023-10-06 18:57:09 -0700

    Finished new font selection script

--- selectfonts.pl
+++ selectfonts.pl
@@ -7,6 +7,7 @@ use strict;
 use Getopt::Std;
 our($opt_d);
 getopts('d');
+my $debug=$opt_d;
 my @requiredChars;
 # Basic latin
 push @requiredChars, 0x0021..0x007E;
@@ -18,22 +19,69 @@ push @requiredChars, 0x0410..0x044F;
 push @requiredChars, 0x0451, 0x0454;
 # Ghe, ghe
 push @requiredChars, 0x0490, 0x0491;
-if ($opt_d) {
-    print STDERR  join(", ", map(chr, @requiredChars)), "\n";
+if ($debug) {
+    print STDERR  "Requrired charecters: ",
+	join(", ", map(chr, @requiredChars)), "\n";
 }
 # loop over fonts
 while (<>) {
     chomp;
-    if ($opt_d) {
-	print STDERR "Woring with font $_\n";
+    my $font=$_;
+    if ($debug) {
+	print STDERR "Working with font $font\n";
     }
-    my $fontfile = `kpsewhich $_`;
+    my @missingChars = GetMissingChars($font, \@requiredChars, $debug);
+    if (!scalar(@missingChars)) {
+	print($font, "\n");
+    } else {
+	if ($debug) {
+	    print STDERR "Missing characters: ",
+		join(", ", map(chr, @missingChars)), "\n";
+	}
+    }
+
+}
+
+sub GetMissingChars {
+    my ($font, $required, $debug) = @_;
+    my @ranges=GetRanges($font, $debug);
+    my @missing;;
+    foreach my $codepoint (@{$required}) {
+	my $found=0;
+	foreach my $range (@ranges) {
+	    my $min=$range->[0];
+	    my $max=$range->[1];
+	    if ($codepoint < $min) {
+		last;
+	    }
+	    if ($codepoint > $max) {
+		next;
+	    }
+	    $found=1;
+	    last;
+	}
+	if (!$found) {
+	    push @missing, $codepoint;
+	}
+    }
+    return(@missing);
+}
+
+sub GetRanges {
+    my ($font, $debug) = @_;
+    my $fontfile = `kpsewhich $font`;
     chomp $fontfile;
-    if ($opt_d) {
+    if ($debug) {
 	print STDERR "Font file $fontfile\n";
     }
-    my $ranges=`fc-query -f '%{charset}' $fontfile`;
-    if ($opt_d) {
-	print STDERR "Ranges $ranges\n";
+    my $rangesStr=`fc-query -f '%{charset}' $fontfile`;
+    my @ranges;
+    foreach my $range (split /\s+/, $rangesStr) {
+	my ($min, $max) = split /-/, $range;
+	if ($max eq '') {
+	    $max=$min;
+	}
+	push @ranges, [ int("0x".$min), int("0x".$max) ];	
     }
+    return @ranges;
 }
